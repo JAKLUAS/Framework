3c60dfa29f63097219f40136984f3e6ff2e66a81
Halil Dikmen <halil.dkmn@gmail.com>
2022-07-01 23:59:15 -0400

    completed gitignore file

---
+++ src/test/java/FrameworkDemo/story1.java
@@ -0,0 +1,47 @@
+package FrameworkDemo;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class story1 extends baseClass{
+
+
+
+    @Test
+    public void Book() {
+
+        driver.get("https://www.delta.com/");
+        driver.findElement(By.id("headPrimary1")).click();
+        driver.findElement(By.xpath("//span[.='From']")) .click();
+        driver.findElement(By.id("search_input")).sendKeys("LGA",Keys.ENTER);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.xpath("//input[@name='arrivalCity']")));
+        driver.findElement(By.xpath("//input[@aria-invalid='false']")).sendKeys("BUR", Keys.ENTER);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.id("selectTripType-val")));
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.id("ui-list-selectTripType1")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.id("input_departureDate_1")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.xpath("//a[@aria-label='13 July 2022, Wednesday']")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.xpath("//a[@aria-label='23 July 2022, Saturday']")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.id("passengers-val")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.id("ui-list-passengers0")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.id("btn-book-submit")));
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+    }
+
